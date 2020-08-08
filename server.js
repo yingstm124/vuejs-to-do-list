b82ff36484da7e943d6a55baaebc26beda0a76ae
Sutimar.p <yingstm124@gmail.com>
2020-08-09 00:12:47 +0700

    add server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+
+const port = process.env.PORT || 8080
+
+const app = express()
+
+app.use(express.static(__dirname + "/dist/"));
+app.get(/.*/, (req, res) => {
+    res.sendFile(__dirname + "/dist/index.html")
+})
+
+app.listen(port, () => {
+    console.log('Port is working')
+})
